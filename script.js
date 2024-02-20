3507eb5ec2560a114341d7ea371daf66f1ddddc2
fauzia789 <fauziafaiza789@gmail.com>
2024-02-20 14:24:41 +0700

    added some js code

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('read-btn').addEventListener('click', function() {
+    document.querySelector('.popular').style.display = 'none';
+    document.querySelector('.places').style.display = 'block'; 
+});
+
+document.getElementById('btn3').addEventListener('click', function() {
+    document.querySelector('.places').style.display = 'none'; 
+    document.querySelector('.popular').style.display = 'block'; 
+    document.querySelector('.popular').style.display = 'flex';
+});
+
+

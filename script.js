910a5ab4aaa73c51ee5782efe57b38498c4c2f22
fauzia789 <fauziafaiza789@gmail.com>
2024-02-20 15:29:19 +0700

    added gotobutton

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ document.getElementById('btn3').addEventListener('click', function() {
 
 
 
+function goToTop() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
+
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var button = document.getElementById("goToTopBtn");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+// Function to increase visitor count
+function increaseVisitorCount() {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.visitorCount) {
+            localStorage.visitorCount = Number(localStorage.visitorCount)+1;
+        } else {
+            localStorage.visitorCount = 1;
+        }
+        document.getElementById("count").innerHTML = localStorage.visitorCount;
+    } else {
+        console.log("Sorry, your browser does not support web storage...");
+    }
+}
+
+// Call the function to increase the count when the page loads
+window.onload = function() {
+    increaseVisitorCount();
+};
